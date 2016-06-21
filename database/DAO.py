2b431b1370f982de442f6670b85dd1997efaecdc
mariane medeiros <marianeaffonsomedeiros@gmail.com>
2016-06-21 18:15:20 -0300

    Leitura do XML.

--- database/DAO.py
+++ database/DAO.py
@@ -17,30 +17,48 @@ import settings
 
 class DAO:
 
-	def __init__(self):
-		self.engine = create_engine(URL(**settings.DATABASE))
-		Session = sessionmaker(bind=self.engine)
-		session = Session()
-		self.readxml = Readxml('../xml/B1.xml')
+    def __init__(self):
+        self.engine = create_engine(URL(**settings.DATABASE))
+        self.readxml = Readxml('../xml/B1.xml')
 
-	def create_tables(self):
-		Base.metadata.create_all(self.engine)
+    def create_tables(self):
+        Base.metadata.create_all(self.engine)
 
-	def insert(self):
-		dict_data = self.readxml.data_wed_conditions()
-		for name in dict_data:
-			print(name['@Name'])
-			#print(name['Predicate'])
-			
-			predicates = name['Predicate']
-			
-			if isinstance(predicates, list):
-				for text in predicates:
-					print(text['#text'])
-			else:
-				print(predicates['#text'])
-		#wed_condition = WED_condition()
-		#session.commit()
+    def insert(self):
+        dict_data = self.readxml.data_wed_conditions()
+
+        for name in dict_data:
+            print(name['@Name'])
+            #print(name['Predicate'])
+            
+            predicates = name['Predicate']
+            if isinstance(predicates, list):
+                for text in predicates:
+                    '''cliente = "invalido" '''
+                    '''- cliente,invalido,='''
+                    vetor = "- " + text['#text'].replace(" = ",",").replace("\"","") + ",="
+                    print(vetor)
+            else:
+                print(predicates['#text'])
+
+            expression = name['Expression']
+            #print (expression)
+            if("AND" in expression):
+                expr = expression.replace(" AND "," ") + " and"
+            elif("OR" in expression):
+                expr = expression.replace(" OR "," ") + " or"
+            else:
+                expr = expression
+
+            print("-- " + expr)
+
+            Session = sessionmaker(bind=self.engine)
+            session = Session()  
+            
+            wed_condition = WED_condition(name=name['@Name'], predicates=vetor, expression=expr)
+            #wed_flow = WED_flow(name="teste",wed_condition=wed_condition);
+            session.add(wed_condition)
+            session.commit()
 
 teste = DAO()
-teste.insert()		
+teste.insert()      
