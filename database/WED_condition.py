f8a9064b499a3cb54b8c4e6591e9aa966dcbc3e3
mariane medeiros <marianeaffonsomedeiros@gmail.com>
2016-07-03 20:39:31 -0300

    aumentando tamanho da string predicates do wed_contidion.

--- database/WED_condition.py
+++ database/WED_condition.py
@@ -7,7 +7,7 @@ class WED_condition(Base):
     __tablename__ = 'wed_condition'
     id = Column(Integer, primary_key = True)
     name = Column(String(50))
-    predicates = Column(String(50))
+    predicates = Column(String(200))
     expression = Column(String(50))
     wed_flow = relationship("WED_flow", back_populates="wed_condition")
     wed_trigger = relationship("WED_trigger", back_populates="wed_condition")
