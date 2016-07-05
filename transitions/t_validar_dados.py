f17f49f8ffaf5fe2ba304c542aba32aad529be10
mariane medeiros <marianeaffonsomedeiros@gmail.com>
2016-07-05 15:35:16 -0300

    código da transiçao.

--- transitions/t_validar_dados.py
+++ transitions/t_validar_dados.py
@@ -16,17 +16,18 @@
         '''
 class t_validar_dados():
     def run(dao, instance, history_entry):
-    	wed_flow_id = dao.session.query(WED_flow).all()[0].id
-    	state_atual = dao.session.query(WED_state).filter_by(id=instance.state_id)
+        wed_flow_id = dao.session.query(WED_flow).all()[0].id
+        # BLOQUEIA O state_atual
+        state_atual = dao.session.query(WED_state).filter_by(id=instance.state_id)
         state = WED_state(cliente='validado', pontos=state_atual.pontos,pedido='validado', instance_id=instance.id, produto=state_atual.produto, pedido=state_atual.pedido, pagamento=state_atual.pagamento)
 
         dao.session.add(state)
         dao.session.commit()
         instance.state_id = state.id
         dao.session.commit()
-
+        # DESBLOQUEIA o state_atual
 
         history_entry.completed_at = datetime.datetime.now()
         history_entry.current_state_id = state_atual.id
         history_entry.final_state_id = state.id
-		dao.session.commit()        
+        dao.session.commit()        
